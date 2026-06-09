19c8de353683eda0b241ea196bd2359e2970e8ce
Naman <namansvj@gmail.com>
2026-06-09 22:15:13 +0530

    Added rectangle drawing

--- main.c
+++ main.c
@@ -1,7 +1,47 @@
 #include <stdio.h>
 
+#define ROWS 20
+#define COLS 40
+
+char canvas[ROWS][COLS];
+
+void initCanvas()
+{
+    for(int i = 0; i < ROWS; i++)
+    {
+        for(int j = 0; j < COLS; j++)
+        {
+            canvas[i][j] = '_';
+        }
+    }
+}
+
+void displayCanvas()
+{
+    for(int i = 0; i < ROWS; i++)
+    {
+        for(int j = 0; j < COLS; j++)
+        {
+            printf("%c", canvas[i][j]);
+        }
+        printf("\n");
+    }
+}
+void drawRectangle(int x, int y, int width, int height)
+{
+    for(int i = x; i < x + height; i++)
+    {
+        for(int j = y; j < y + width; j++)
+        {
+            canvas[i][j] = '*';
+        }
+    }
+}
 int main()
 {
-    printf("Hello World\n");
+    initCanvas();
+    drawRectangle(2, 5, 10, 4);
+    displayCanvas();
+
     return 0;
 }
